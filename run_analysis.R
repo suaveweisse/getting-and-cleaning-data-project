c27e4f925deca09094c9567abb72ab531d2aaaf4
suaveweisse <rico.p.torres@gmail.com>
2016-05-02 00:45:37 -0400

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+## Set file and directory names
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, "./UCI.zip")
+unzip("UCI.zip")
+dataDir <- "./UCI HAR Dataset/"
+
+x_test <- read.table(paste(dataDir, "test/X_test.txt", sep = ""), header = F)
+x_train <- read.table(paste(dataDir, "train/X_train.txt", sep = ""), header = F)
+activity_labels <- read.table(paste(dataDir, "activity_labels.txt", sep = ""), header = F, col.names = c("activityID", "activityType"))[,2]
+y_test <- read.table(paste(dataDir, "test/y_test.txt", sep = ""), header = F, col.names = "activityType")
+y_train <- read.table(paste(dataDir, "train/y_train.txt", sep = ""), header = F, col.names = "activityType")
+subject_test <- read.table(paste(dataDir, "test/subject_test.txt", sep = ""), header = F, col.names = "subjectID")
+subject_train <- read.table(paste(dataDir, "train/subject_train.txt", sep = ""), header = F, col.names = "subjectID")
+## read features, create new column for revised feature names, clean up feature names
+features <- read.table(paste(dataDir, "features.txt", sep = ""), header = F, stringsAsFactors = F, col.names = c("featureID","featureName"))
+features$featureNameNew <- features$featureName
+features$featureNameNew <- gsub("\\()","",features$featureNameNew)
+features$featureNameNew <- gsub("std","StdDev",features$featureNameNew)
+features$featureNameNew <- gsub("mean","Mean",features$featureNameNew)
+features$featureNameNew <- gsub("^(t)","time",features$featureNameNew)
+features$featureNameNew <- gsub("^(f)","freq",features$featureNameNew)
+features$featureNameNew <- gsub("([Gg]ravity)","Gravity",features$featureNameNew)
+features$featureNameNew <- gsub("([Bb]ody)","Body",features$featureNameNew)
+features$featureNameNew <- gsub("[Gg]yro","Gyro",features$featureNameNew)
+features$featureNameNew <- gsub("Acc","Accel", features$featureNameNew)
+columnstokeep <- grep("mean|std", features$featureName)
+
+# create 'x' data set
+x_data <- rbind(x_train, x_test)
+colnames(x_data) <- features$featureNameNew
+x_data <- x_data[,columnstokeep]
+
+# asssign the activity name
+y_test$activityName = activity_labels[y_test[,1]]
+y_train$activityName = activity_labels[y_train[,1]]
+# remove unnecessary variable
+y_test$activityType = NULL
+y_train$activityType = NULL
+# create 'y' data set
+y_data <- rbind(y_train, y_test)
+# create 'subject' data set
+subject_data <- rbind(subject_train, subject_test)
+all_data_tidy <- cbind(x_data, y_data, subject_data)
+
+all_data_tidy_subject_avg <- aggregate(all_data_tidy, by=list(activity = all_data_tidy$activityName, subject = all_data_tidy$subjectID), mean)
+all_data_tidy_subject_avg$activityName = NULL
+all_data_tidy_subject_avg$subjectID = NULL
+colnames(all_data_tidy_subject_avg)[1] <- "activityName"
+colnames(all_data_tidy_subject_avg)[2] <- "subjectID"
